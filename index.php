df04b20aaf2149ace08c8cd09a4cc43000c78252
Davide Ponzini <davide.ponzini95@gmail.com>
2023-10-19 15:51:47 +0200

    updated footer & formatted server values

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
         <?php include('../components/ads_banner.html'); ?>
     </div>
 
-    <?php include('../components/footer.html'); ?>
+    <?php include('../components/footer.php'); ?>
 
     <!-- Boostrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- server.php
+++ server.php
@@ -1,5 +1,17 @@
-<?php
-    foreach($_SERVER as $key => $val) {
-        echo '[' . $key .'] => ' . $val . '<br>';
-    }
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$_SERVER</title>
+</head>
+<body>
+    <pre><?php
+            foreach($_SERVER as $key => $val) {
+                echo '<b>[' . $key .'] => </b>' . $val . '<br>';
+            }
+    ?></pre>
+</body>
+</html>
+
+
